fe1091f86cd5230e97f5e4c89fc02d08fb66565a
João Fernando <joaofernando.costa@corp.globo.com>
2017-04-13 00:35:41 -0300

    Add 'ShortenerApp.js' component

--- app/components/ShortenerApp.js
+++ app/components/ShortenerApp.js
@@ -2,18 +2,16 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 import Nav from "./Nav";
+import Shortener from "./Shortener";
 
 
 export default class ShortenerApp extends Component {
 
-  componentDidMount() {
-    console.log('componentDidMount');
-  }
-
   render() {
     return (
       <div>
-        <Nav />
+        <Nav/>
+        <Shortener/>
       </div>
     );
   }
